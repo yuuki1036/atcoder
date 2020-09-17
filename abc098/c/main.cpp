eae9c738187c73d161e4df77fae512549e4f46f7
yuuki1036 <yuuki1036@gmail.com>
2020-09-17 12:37:38 +0900

    backup

--- abc098/c/main.cpp
+++ abc098/c/main.cpp
@@ -1,8 +1,19 @@
 #include <bits/stdc++.h>
 using namespace std;
 int main(){
-  
-
-
-
+  int n;
+  cin >> n;
+  string s;
+  cin >> s;
+  int sum = 0;
+  for(int i=1; i<n; ++i) if(s[i] == 'E') ++sum;
+  int ans = sum;
+  for(int i=1; i<n; ++i){
+    int tmp = 0;
+    if(s[i-1] == 'W') ++tmp; 
+    if(s[i] == 'E') --tmp;
+    sum += tmp;
+    ans = min(ans, sum); 
+  }
+  cout << ans << endl;
 }
--- abc178/d/main.cpp
+++ abc178/d/main.cpp
@@ -1,8 +1,15 @@
 #include <bits/stdc++.h>
 using namespace std;
+using ll = long long;
+const ll mod = 1e9+7;
 int main(){
-  
-
-
-
+  ll s;
+  cin >> s;
+  if(s < 3){ cout << 0 << endl; return 0; }
+  ll dp[2000];
+  dp[0] = dp[1] = dp[2] = 1;
+  for(int i=3; i<=s-3; ++i){
+    dp[i] = (dp[i-1]+dp[i-3])%mod;
+  }
+  cout << dp[s-3] << endl;
 }
