eb48a8200031cda6e9ef21bfe22f52315eef939d
yuuki1036 <yuuki1036@gmail.com>
2020-09-19 09:59:26 +0900

    backup

--- abc079/c/main.cpp
+++ abc079/c/main.cpp
@@ -1,7 +1,22 @@
 #include <bits/stdc++.h>
 using namespace std;
 int main(){
- 
-
-
+  string s;
+  cin >> s;
+  for(int bit=0; bit<(1<<3); ++bit){
+    int sum = s[0]-'0';
+    for(int i=1; i<=3; ++i){
+      if(bit & (1<<(i-1))) sum += s[i]-'0';
+      else sum -= s[i]-'0';
+    }
+    if(sum == 7){
+      cout << s[0];
+      for(int i=1; i<=3; ++i){
+        if(bit & (1<<(i-1))) cout << "+" << s[i];
+        else cout << "-" << s[i];
+      }
+      cout << "=7" << endl;
+      return 0;
+    }
+  }
 }
