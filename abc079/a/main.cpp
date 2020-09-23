f299f0821212158d09c4df1481cafeecae5a054e
yuuki1036 <yuuki1036@gmail.com>
2020-09-23 22:06:56 +0900

    backup

--- abc079/a/main.cpp
+++ abc079/a/main.cpp
@@ -1,9 +1,13 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define rep(i, n) for (int i = 0; i < (int)(n); i++)
 int main(){
-
-
-
-
+  string s;
+  cin >> s;
+  for(int i=0; i<2; ++i) {
+    if(s[i] == s[i+1] && s[i] == s[i+2]){
+      cout << "Yes" << endl;
+      return 0;
+    }
+  };
+  cout << "No" << endl;
 }
--- abc079/b/main.cpp
+++ abc079/b/main.cpp
@@ -1,9 +1,11 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+using ll = long long;
 int main(){
-
-
-
-
+  ll n;
+  cin >> n;
+  ll a[90];
+  a[0] = 2; a[1] = 1;
+  for(int i=2; i<n+1; ++i) a[i] = a[i-1]+a[i-2];
+  cout << a[n] << endl;
 }
