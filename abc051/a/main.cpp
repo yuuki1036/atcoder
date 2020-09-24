fc35374c6dc67e6d5ea0d5f7bc1e7c6a8f811008
yuuki1036 <yuuki1036@gmail.com>
2020-09-24 23:56:57 +0900

    backup

--- abc051/a/main.cpp
+++ abc051/a/main.cpp
@@ -1,8 +1,11 @@
 #include <bits/stdc++.h>
 using namespace std;
 int main(){
-  
-
-
-
+  string s;
+  cin >> s;
+  for(int i=0; i<s.size(); ++i){
+    if(s[i] == ',') cout << " ";
+    else cout << s[i];
+  }
+  cout << endl;
 }
--- abc051/c/main.cpp
+++ abc051/c/main.cpp
@@ -1,8 +1,19 @@
 #include <bits/stdc++.h>
 using namespace std;
 int main(){
-  
-
-
-
+  int sx, sy, tx, ty;
+  cin >> sx >> sy >> tx >> ty;
+  int dx = tx-sx, dy = ty-sy;
+  for(int i=0; i<dx; ++i) cout << "R";
+  for(int i=0; i<dy; ++i) cout << "U";
+  for(int i=0; i<dx; ++i) cout << "L";
+  for(int i=0; i<dy; ++i) cout << "D";
+  cout << "L";
+  for(int i=0; i<dy+1; ++i) cout << "U";
+  for(int i=0; i<dx+1; ++i) cout << "R";
+  cout << "D";
+  cout << "R";
+  for(int i=0; i<dy+1; ++i) cout << "D";
+  for(int i=0; i<dx+1; ++i) cout << "L";
+  cout << "U" << endl;
 }
