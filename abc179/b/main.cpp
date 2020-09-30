c06c620970fd8f6eee072bd5a9b454d96be7f4ea
yuuki1036 <yuuki1036@gmail.com>
2020-09-30 23:43:26 +0900

    backuo

--- abc179/b/main.cpp
+++ abc179/b/main.cpp
@@ -1,8 +1,20 @@
 #include <bits/stdc++.h>
 using namespace std;
 int main(){
-  
-
-
-
+  int n, z = 0;
+  cin >> n;
+  while(n--){
+    int a, b;
+    cin >> a >> b;
+    if(a == b){
+      z += 1;
+      if(z == 3){
+        cout << "Yes" << endl;
+        return 0;
+      }
+    }else{
+      z = 0;
+    }
+  }
+  cout << "No" << endl;
 }
