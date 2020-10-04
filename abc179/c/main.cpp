e677033dcebd93ca525ce4de2f8cc767415f556a
yuuki1036 <yuuki1036@gmail.com>
2020-10-04 10:22:02 +0900

    backup

--- abc179/c/main.cpp
+++ abc179/c/main.cpp
@@ -1,8 +1,10 @@
 #include <bits/stdc++.h>
 using namespace std;
 int main(){
-  
-
-
-
+  int n;
+  cin >> n;
+  --n;
+  long long ans = 0;
+  for(int i=1; i<=n; ++i) ans += n/i;
+  cout << ans << endl;
 }
