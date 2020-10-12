ec9265be2c55398bd8ff76019261e0daf1bda335
yuuki1036 <yuuki1036@gmail.com>
2020-10-12 12:36:02 +0900

    backup

--- abc043/c/main.cpp
+++ abc043/c/main.cpp
@@ -1,8 +1,18 @@
 #include <bits/stdc++.h>
 using namespace std;
 int main(){
-  
-
-
-
+  int n;
+  cin >> n;
+  int a[n];
+  for (int i=0; i<n; ++i) cin >> a[i];
+  int ans = 1e9;
+  for(int rep=-100; rep<=100; ++rep){
+    int cst = 0;
+    for(int i=0; i<n; ++i){
+      if(a[i] == rep) continue;
+      cst += pow(a[i]-rep, 2);
+    }
+    ans = min(ans, cst);
+  }
+  cout << ans << endl;
 }
