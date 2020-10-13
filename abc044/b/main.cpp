40b27709960d431caa048204cf202b9760de01e9
yuuki1036 <yuuki1036@gmail.com>
2020-10-13 22:57:57 +0900

    backup

--- abc044/b/main.cpp
+++ abc044/b/main.cpp
@@ -1,8 +1,12 @@
 #include <bits/stdc++.h>
 using namespace std;
 int main(){
-  
-
-
-
+  string s;
+  cin >> s;
+  vector<int> a(100);
+  for(int i=0; i<s.size(); ++i) ++a[s[i]-'0'];
+  for(int i=0; i<=100; ++i){
+    if(a[i]%2 != 0){ cout << "No" << endl; return 0; }
+  }
+  cout << "Yes" << endl;
 }
