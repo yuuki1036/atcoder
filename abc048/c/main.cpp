28e589d8d552edf782f89d14c65abaf905ec70a8
yuuki1036 <yuuki1036@gmail.com>
2020-10-16 04:28:23 +0900

    backup

--- abc048/c/main.cpp
+++ abc048/c/main.cpp
@@ -1,8 +1,21 @@
 #include <bits/stdc++.h>
 using namespace std;
 int main(){
-  
-
-
-
+  int n, x;
+  cin >> n >> x;
+  int a[n];
+  for(int i=0; i<n; ++i) cin >> a[i];
+  long long res = 0;
+  if(a[0] > x){
+    res += a[0]-x;
+    a[0] = x;
+  }
+  for(int i=0; i<n-1; ++i){
+    int sum = a[i]+a[i+1];
+    if(sum > x){
+      res += sum-x;
+      a[i+1] -= sum-x;
+    }
+  }
+  cout << res << endl;
 }
---
+++ abc049/b/main.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+  int h, w;
+  cin >> h >> w;
+  string s;
+  while(h--){
+    cin >> s;
+    cout << s << endl;
+    cout << s << endl;
+  }
+}
