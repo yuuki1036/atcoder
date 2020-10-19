668c98a0ff1d2dc0cad1c0e79c7d20420e0c3780
yuuki1036 <yuuki1036@gmail.com>
2020-10-20 08:22:25 +0900

    backup

--- abc052/b/main.cpp
+++ abc052/b/main.cpp
@@ -1,8 +1,15 @@
 #include <bits/stdc++.h>
 using namespace std;
 int main(){
-  
-
-
-
+  int n;
+  cin >> n;
+  string s;
+  cin >> s;
+  int res = 0, now = 0;
+  for(int i=0; i<n; ++i){
+    if(s[i] == 'I') ++now; 
+    else --now;
+    res = max(res, now);
+  }
+  cout << res << endl;
 }
