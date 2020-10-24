17a2c9d538d68eedc2eae432fb0e68b394340220
yuuki1036 <yuuki1036@gmail.com>
2020-10-24 18:24:58 +0900

    backup

--- abc053/c/main.cpp
+++ abc053/c/main.cpp
@@ -1,8 +1,12 @@
 #include <bits/stdc++.h>
 using namespace std;
+using ll = long long;
 int main(){
-  
-
-
-
+  ll x;
+  cin >> x;
+  ll ans = x/11*2;
+  int remain = x%11;
+  if(remain > 6) ans += 2;
+  else if(remain <= 6 && remain > 0) ++ans;
+  cout << ans << endl;
 }
