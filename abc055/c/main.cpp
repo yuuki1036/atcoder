1925dad8e85e169fe0c75a9cc8eff5487eff893b
yuuki1036 <yuuki1036@gmail.com>
2020-10-31 23:42:02 +0900

    backup

--- abc055/c/main.cpp
+++ abc055/c/main.cpp
@@ -1,8 +1,12 @@
 #include <bits/stdc++.h>
 using namespace std;
+using ll = long long;
 int main(){
-  
-
-
-
+  ll n, m;
+  cin >> n >> m;
+  ll res = 0;
+  ll need = n*2;
+  if (m < need) res = m/2;
+  else res = n+((m-need)/4);
+  cout << res << endl;
 }
