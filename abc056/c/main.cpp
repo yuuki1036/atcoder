319f098afc9e84d7b486165f8b5b8bbcf47aaf5a
yuuki1036 <yuuki1036@gmail.com>
2020-11-05 23:05:11 +0900

    backup

--- abc056/c/main.cpp
+++ abc056/c/main.cpp
@@ -1,8 +1,13 @@
 #include <bits/stdc++.h>
 using namespace std;
+using ll = long long;
 int main(){
-  
-
-
-
+  ll n;
+  cin >> n;
+  ll now = 1, res = 1;
+  while(now < n){
+    ++res;
+    now += res;
+  }
+  cout << res << endl;
 }
