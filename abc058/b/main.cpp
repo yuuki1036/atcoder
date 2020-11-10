1120300144fc742a38bee483bc8458ac62e95f44
yuuki1036 <yuuki1036@gmail.com>
2020-11-10 23:42:22 +0900

    backup

--- abc058/b/main.cpp
+++ abc058/b/main.cpp
@@ -1,8 +1,9 @@
 #include <bits/stdc++.h>
 using namespace std;
 int main(){
-  
-
-
-
+  string o, e;
+  cin >> o >> e;
+  int ol = o.size(), el = e.size();
+  for(int i=0; i<el; ++i) cout << o[i] << e[i];
+  if(ol > el) cout << o[ol-1];
 }
