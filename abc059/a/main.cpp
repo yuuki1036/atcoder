70b73f2910be1f0713b0b54ce4859d5ec3363c24
yuuki1036 <yuuki1036@gmail.com>
2020-11-13 01:00:46 +0900

    backup

--- abc059/a/main.cpp
+++ abc059/a/main.cpp
@@ -1,8 +1,11 @@
 #include <bits/stdc++.h>
 using namespace std;
 int main(){
-  
-
-
-
+  string S[3];
+  for(int i=0; i<3; ++i){
+    string s;
+    cin >> s;
+    char c(s[0]-32);
+    cout << c;
+  }
 }
