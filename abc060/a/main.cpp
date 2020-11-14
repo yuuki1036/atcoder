a7dfa8cbbcdbab195d9eb10183cbdc479d2dcc05
yuuki1036 <yuuki1036@gmail.com>
2020-11-14 23:51:52 +0900

    backup

--- abc060/a/main.cpp
+++ abc060/a/main.cpp
@@ -1,8 +1,8 @@
 #include <bits/stdc++.h>
 using namespace std;
 int main(){
-  
-
-
-
+  string a, b, c;
+  cin >> a >> b >> c;
+  int al = a.size(), bl = b.size(), cl = c.size();
+  cout << (a[al-1] == b[0] && b[bl-1] == c[0] ? "YES" : "NO") << endl;
 }
