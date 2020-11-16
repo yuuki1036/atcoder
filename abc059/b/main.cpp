f14d5cf6add58386b9fe495357e3a4c11da7ad64
yuuki1036 <yuuki1036@gmail.com>
2020-11-16 12:50:47 +0900

    backup

--- abc059/b/main.cpp
+++ abc059/b/main.cpp
@@ -1,8 +1,15 @@
 #include <bits/stdc++.h>
 using namespace std;
 int main(){
-  
-
-
-
+  string a, b;
+  cin >> a >> b;
+  int al = a.size(), bl = b.size();
+  if(a == b){ cout << "EQUAL" << endl; return 0; }
+  if(al > bl){ cout << "GREATER" << endl; return 0; }
+  if(al < bl){ cout << "LESS" << endl; return 0; }
+  for(int i=0; i<al; ++i){
+    int an = a[i]+0, bn = b[i]+0;
+    if(an > bn){ cout << "GREATER" << endl; return 0; }
+    if(an < bn){ cout << "LESS" << endl; return 0; }
+  }
 }
