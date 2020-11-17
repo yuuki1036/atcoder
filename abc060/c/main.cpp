8382443d1aa3c9859bb408eb69cbe063ded7e368
yuuki1036 <yuuki1036@gmail.com>
2020-11-17 23:42:37 +0900

    backup

--- abc060/c/main.cpp
+++ abc060/c/main.cpp
@@ -1,8 +1,14 @@
 #include <bits/stdc++.h>
 using namespace std;
 int main(){
-  
-
-
-
+  int n, t;
+  cin >> n >> t;
+  int A[n];
+  for(int i=0; i<n; ++i) cin >> A[i];
+  long long ans = t;
+  for(int i=1; i<n; ++i){
+    int b = A[i]-A[i-1];
+    ans += b < t ? b : t;
+  }
+  cout << ans << endl;
 }
