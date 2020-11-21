663adc35d4d32b92db7949ba6552c87e5c15f3eb
yuuki1036 <yuuki1036@gmail.com>
2020-11-21 12:44:19 +0900

    backyp

--- abc062/a/main.cpp
+++ abc062/a/main.cpp
@@ -1,9 +1,15 @@
 #include <bits/stdc++.h>
 using namespace std;
 int main(){
-
-
-
-
+  const vector<int>v{4, 6, 9, 11};
+  int x, y, gx, gy;
+  cin >> x >> y;
+  if(find(v.begin(), v.end(), x) != v.end()) gx = 2;
+  else if(x == 2) gx = 3;
+  else gx = 1;
+  if(find(v.begin(), v.end(), y) != v.end()) gy = 2;
+  else if(y == 2) gy = 3;
+  else gy = 1;
+  cout << (gx == gy ? "Yes" : "No") << endl;
 }
 
