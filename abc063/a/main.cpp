0fb62d05d09141294e4f7f3d349b960cb9019eff
yuuki1036 <yuuki1036@gmail.com>
2020-11-22 10:20:48 +0900

    backup

---
+++ abc063/a/main.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+  int a, b;
+  cin >> a >> b;
+  int sum = a+b;
+  if(sum >= 10) cout << "error" << endl;
+  else cout << sum << endl;
+}
