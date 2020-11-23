4f0085e21d28a607c61e61caef2ee6f345dca8f8
yuuki1036 <yuuki1036@gmail.com>
2020-11-24 01:34:54 +0900

    backup

--- abc063/b/main.cpp
+++ abc063/b/main.cpp
@@ -1,8 +1,13 @@
 #include <bits/stdc++.h>
 using namespace std;
 int main(){
-  
-
-
-
+  string s;
+  cin >> s;
+  set<char> C;
+  string ans = "yes";
+  for(int i=0; i<s.size(); ++i){
+    if(C.find(s[i]) != C.end()) ans = "no";
+    else C.insert(s[i]);
+  }
+  cout << ans << endl;
 }
