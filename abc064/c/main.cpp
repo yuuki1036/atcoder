9a6454bd67912cbbb67c88dd4d1b02b5386d4d52
yuuki1036 <yuuki1036@gmail.com>
2020-12-02 23:59:44 +0900

    bacjup

--- abc064/c/main.cpp
+++ abc064/c/main.cpp
@@ -1,8 +1,17 @@
 #include <bits/stdc++.h>
 using namespace std;
 int main(){
-  
-
-
-
+  int n;
+  cin >> n;
+  int a[n];
+  for(int i=0; i<n; ++i) cin >> a[i];
+  set<int>s;
+  int addColor = 0;
+  for(int i=0; i<n; ++i){
+    if(a[i] >= 3200) ++addColor;
+    else s.insert(a[i]/400);
+  }
+  int ansMin = s.size() == 0 ? 1 : s.size();
+  int ansMax = s.size() + addColor;
+  cout << ansMin << " " << ansMax << endl; 
 }
