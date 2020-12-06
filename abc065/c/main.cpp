0cbfc7d23e3e801734e5fb165f5e07064f495257
yuuki1036 <yuuki1036@gmail.com>
2020-12-06 11:46:31 +0900

    backup

--- abc065/c/main.cpp
+++ abc065/c/main.cpp
@@ -1,9 +1,25 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+using ll = long long;
+const ll MOD = 1e9+7;
+ll f(int x){
+  ll res = 1;
+  while(x > 0){
+    res = res*x%MOD;
+    --x;
+  }
+  return res;
+}
+
 int main(){
-
-
-
-
+  int n, m;
+  cin >> n >> m;
+  ll ans = 0;
+  if(n != m && n+1 != m && n != m+1){
+    cout << ans << endl;
+    return 0;
+  }
+  ans = f(n)*f(m)%MOD;
+  if(n == m) ans = ans*2%MOD;
+  cout << ans << endl;
 }
