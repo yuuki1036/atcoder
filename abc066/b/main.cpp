9718cca60afc8f92b083c4fc014bb3a2b01b819f
yuuki1036 <yuuki1036@gmail.com>
2020-12-09 20:18:19 +0900

    backup

--- abc066/b/main.cpp
+++ abc066/b/main.cpp
@@ -1,8 +1,13 @@
 #include <bits/stdc++.h>
 using namespace std;
 int main(){
-  
-
-
-
+  string s;
+  cin >> s;
+  int l = s.size()/2-1;
+  int ans = 0;
+  for(int i=1; i<=l; ++i){
+    string l = s.substr(0, i), r = s.substr(i, i);
+    if(l == r) ans = max(ans, i*2);
+  }
+  cout << ans << endl;
 }
