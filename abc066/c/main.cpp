d3665bd35474cfe9f3fda3ba83a4feb08466e7a6
yuuki1036 <yuuki1036@gmail.com>
2020-12-12 10:35:43 +0900

    backup

--- abc066/c/main.cpp
+++ abc066/c/main.cpp
@@ -1,8 +1,24 @@
 #include <bits/stdc++.h>
 using namespace std;
+using ll = long long;
 int main(){
-  
-
-
-
+  int n;
+  cin >> n;
+  ll a[n];
+  for(int i=0; i<n; ++i) cin >> a[i];
+  deque<ll> deq;
+  bool t = n%2 == 0 ? true : false;
+  for(int i=0; i<n; ++i){
+    if(t){
+      deq.push_back(a[i]);
+      t = false;
+    }else{
+      deq.push_front(a[i]);
+      t = true;
+    }
+  }
+  for(int i=0; i<n; ++i){
+    cout << deq[i];
+    if(i+1 < n) cout << " "; 
+  }
 }
