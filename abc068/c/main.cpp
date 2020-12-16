86ae51384c74f40691eaa111ae8c07fba26891c6
yuuki1036 <yuuki1036@gmail.com>
2020-12-16 19:12:00 +0900

    backup

--- abc068/c/main.cpp
+++ abc068/c/main.cpp
@@ -1,8 +1,21 @@
 #include <bits/stdc++.h>
 using namespace std;
 int main(){
-  
-
-
-
+  int n, m;
+  cin >> n >> m;
+  vector<pair<int, int>> p(m);
+  set<int> s;
+  for(auto& [a, b] : p) cin >> a >> b;
+  for(int i=0; i<m; ++i){
+    auto [a, b] = p[i];
+    if(a == 1) s.insert(b);
+  }
+  for(int i=0; i<m; ++i){
+    auto [a, b] = p[i];
+    if(s.find(a) != s.end() && b == n){
+      cout << "POSSIBLE" << endl;
+      return 0;
+    }
+  }
+  cout << "IMPOSSIBLE" << endl;
 }
