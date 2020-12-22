f4fa0ebd75d3f119cbbe7af0e4b20e3645ccebce
yuuki1036 <yuuki1036@gmail.com>
2020-12-22 23:48:09 +0900

    backup

--- abc069/c/main.cpp
+++ abc069/c/main.cpp
@@ -1,8 +1,15 @@
 #include <bits/stdc++.h>
 using namespace std;
 int main(){
-  
-
-
-
+  int n;
+  cin >> n;
+  int a = 0, b = 0, c = 0;
+  for(int i=0; i<n; ++i){
+    int tmp;
+    cin >> tmp;
+    if(tmp == 2) ++b;
+    else if(tmp%4 == 0) ++a;
+    else ++c;
+  }
+  cout<<a<<" "<<b<<" "<<c<<endl;
 }
--- abc070/a/main.cpp
+++ abc070/a/main.cpp
@@ -1,8 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 int main(){
-  
-
-
-
+  string n;
+  cin >> n;
+  cout << (n[0] == n[2] ? "Yes" : "No") << endl;
 }
