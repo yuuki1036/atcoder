a0468b7522d65ec699694570863bcbf6c54a590b
yuuki1036 <yuuki1036@gmail.com>
2020-12-23 18:52:53 +0900

    backup

--- abc069/c/main.cpp
+++ abc069/c/main.cpp
@@ -1,15 +1,17 @@
 #include <bits/stdc++.h>
 using namespace std;
+using ll = long long;
 int main(){
   int n;
   cin >> n;
   int a = 0, b = 0, c = 0;
   for(int i=0; i<n; ++i){
-    int tmp;
+    ll tmp;
     cin >> tmp;
-    if(tmp == 2) ++b;
-    else if(tmp%4 == 0) ++a;
+    if(tmp%4 == 0) ++a;
+    else if(tmp%2 == 0) ++b;
     else ++c;
   }
-  cout<<a<<" "<<b<<" "<<c<<endl;
+  if(b > 0) cout << (a >= c ? "Yes" : "No") << endl;
+  else cout << (a+1 >= c ? "Yes" : "No") << endl;
 }
