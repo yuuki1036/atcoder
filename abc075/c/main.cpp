aadb359f1323f5424f841b1bda5923c3433f0d95
yuuki1036 <yuuki1036@gmail.com>
2021-01-05 02:14:42 +0900

    backup

--- abc075/c/main.cpp
+++ abc075/c/main.cpp
@@ -1,8 +1,36 @@
 #include <bits/stdc++.h>
 using namespace std;
+const int lim = 50;
+int n, m;
+int a[lim], b[lim];
+bool g[lim][lim];
+bool v[lim];
+
+void dfs(int x){
+  v[x] = true;
+  for(int nx=0; nx<n; ++nx){
+    if(v[nx]) continue;
+    if(!g[x][nx]) continue;
+    dfs(nx);
+  }
+}
+
 int main(){
-  
-
-
-
+  cin >> n >> m;
+  for(int i=0; i<m; ++i){
+    cin >> a[i] >> b[i];
+    --a[i], --b[i];
+    g[a[i]][b[i]] = g[b[i]][a[i]] = true;
+  }
+  int ans = 0;
+  for(int i=0; i<m; ++i){
+    g[a[i]][b[i]] = g[b[i]][a[i]] = false;
+    for(int j=0; j<n; ++j) v[j] = false;
+    dfs(0);
+    bool bridge = false;
+    for(int j=0; j<n; ++j) if(!v[j]) bridge = true;
+    if(bridge) ++ans;
+    g[a[i]][b[i]] = g[b[i]][a[i]] = true;
+  }
+  cout << ans << endl;
 }
