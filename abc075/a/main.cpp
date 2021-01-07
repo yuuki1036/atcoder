9b8f977944a0ad55ab21871e0e9b4a46629effa5
yuuki1036 <yuuki1036@gmail.com>
2021-01-07 12:47:00 +0900

    backup

--- abc075/a/main.cpp
+++ abc075/a/main.cpp
@@ -1,8 +1,10 @@
 #include <bits/stdc++.h>
 using namespace std;
 int main(){
-  
-
-
-
+  int a, b, c, x;
+  cin >> a >> b >> c;
+  if(a == b) x = c;
+  else if(a == c) x = b;
+  else x = a;
+  cout << x << endl;
 }
