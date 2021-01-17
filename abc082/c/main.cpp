e577ded1a3e1f23626ad5c5fff4ce60c0edc48fb
yuuki1036 <yuuki1036@gmail.com>
2021-01-17 10:23:23 +0900

    backup

--- abc082/c/main.cpp
+++ abc082/c/main.cpp
@@ -1,8 +1,20 @@
 #include <bits/stdc++.h>
 using namespace std;
+using ll = long long;
 int main(){
-  
-
-
-
+  int n;
+  cin >> n;
+  map<ll, ll> m;
+  for(int i=0; i<n; ++i){
+    ll tmp;
+    cin >> tmp;
+    if(m.find(tmp) != m.end()) ++m[tmp];
+    else m[tmp] = 1;
+  }
+  int ans = 0;
+  for(auto& [k, v] : m){
+    if(k < v) ans += v-k;
+    else if(k > v) ans += v;
+  }
+  cout << ans <<endl;
 }
