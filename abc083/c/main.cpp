f4249ce0579e544e9ddc7bbb226d029f69bb654a
yuuki1036 <yuuki1036@gmail.com>
2021-01-23 20:48:30 +0900

    backup

--- abc083/c/main.cpp
+++ abc083/c/main.cpp
@@ -1,8 +1,14 @@
 #include <bits/stdc++.h>
 using namespace std;
+using ll = long long;
 int main(){
-  
-
-
-
+  ll x, y;
+  cin >> x >> y;
+  ll ans = 1;
+  ll a = x*2;
+  while(a <= y){
+    a *= 2;
+    ++ans;
+  }
+  cout << ans <<endl;
 }
--- abc084/b/main.cpp
+++ abc084/b/main.cpp
@@ -1,8 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 int main(){
-  
-
-
-
+  string a, b, s;
+  cin >> a >> b >> s;
+  cout << (regex_match(s, regex("\\d{" + a + "}-\\d{" + b + "}")) ? "Yes" : "No") << endl;
 }
