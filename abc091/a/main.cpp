4ed7250c82a6d55a59694fb90b9aa4bb1900157c
yuuki1036 <yuuki1036@gmail.com>
2021-02-01 23:07:48 +0900

    backup

--- abc091/a/main.cpp
+++ abc091/a/main.cpp
@@ -1,9 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define rep(i, n) for (int i = 0; i < (int)(n); i++)
 int main(){
-
-
-
-
+  int a, b, c;
+  cin >> a >> b >> c;
+  cout << (a+b < c ? "No" : "Yes") << endl;
 }
