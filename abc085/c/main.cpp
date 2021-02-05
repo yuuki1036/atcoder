35f91a6d5a8dd94e3299e2428e1cc6cfe529b673
yuuki1036 <yuuki1036@gmail.com>
2021-02-05 21:22:34 +0900

    backup

--- abc085/c/main.cpp
+++ abc085/c/main.cpp
@@ -1,9 +1,15 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define rep(i, n) for (int i = 0; i < (int)(n); i++)
 int main(){
-
-
-
-
+  int n, y;
+  cin >> n >> y;
+  for(int i=0; i<=n; ++i){
+    for(int j=0; i+j<=n; ++j){
+      if (i*10000+j*5000+(n-i-j)*1000 == y) {
+        cout << i << " " << j << " " << n-i-j << endl;
+        return 0;
+      }
+    }
+  }
+  cout << "-1 -1 -1" << endl;
 }
