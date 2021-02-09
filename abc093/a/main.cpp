70435bef17a0552cdcf1991806424d166920918d
yuuki1036 <yuuki1036@gmail.com>
2021-02-09 23:56:53 +0900

    backup

--- abc093/a/main.cpp
+++ abc093/a/main.cpp
@@ -1,8 +1,9 @@
 #include <bits/stdc++.h>
 using namespace std;
 int main(){
-  
-
-
-
+  string S;
+  cin >> S;
+  int n = 0;
+  for(char s : S) n += s-'a';
+  cout << (n == 3 ? "Yes" : "No") << endl;
 }
