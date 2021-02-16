7505ead2bbb4787d2d8b6ce248c804d05e78474a
yuuki1036 <yuuki1036@gmail.com>
2021-02-17 06:29:28 +0900

    backup

--- abc087/b/main.cpp
+++ abc087/b/main.cpp
@@ -1,8 +1,15 @@
 #include <bits/stdc++.h>
 using namespace std;
 int main(){
-  
-
-
-
+  int a, b, c, x;
+  cin >> a >> b >> c >> x;
+  int ans = 0;
+  for(int i=0; i<=a; ++i){
+    for(int j=0; i<=b; ++j){
+      for(int k=0; k<=c; ++k){
+        if(500*a+100*b+50*c == x) ++ans;
+      }
+    }
+  }
+  cout << ans;
 }
