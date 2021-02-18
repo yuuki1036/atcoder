dc4b62d1430ab4eea1b12f0b41c1a6680cdf9544
yuuki1036 <yuuki1036@gmail.com>
2021-02-18 22:31:55 +0900

    backup

--- abc087/b/main.cpp
+++ abc087/b/main.cpp
@@ -5,11 +5,11 @@ int main(){
   cin >> a >> b >> c >> x;
   int ans = 0;
   for(int i=0; i<=a; ++i){
-    for(int j=0; i<=b; ++j){
+    for(int j=0; j<=b; ++j){
       for(int k=0; k<=c; ++k){
-        if(500*a+100*b+50*c == x) ++ans;
+        if(500*i+100*j+50*k == x) ++ans;
       }
     }
   }
-  cout << ans;
+  cout << ans << endl;
 }
