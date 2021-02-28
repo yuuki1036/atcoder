2c5ff15b1e3ef0f0c21581d2f38f2f46edecb2d3
yuuki1036 <yuuki1036@gmail.com>
2021-02-28 23:25:20 +0900

    backup

--- abc092/b/main.cpp
+++ abc092/b/main.cpp
@@ -1,8 +1,16 @@
 #include <bits/stdc++.h>
 using namespace std;
 int main(){
-  
-
-
-
+  int n, d, x, a;
+  cin >> n >> d >> x;
+  int eat = 0;
+  for(int i=0; i<n; ++i){
+    cin >> a;
+    for(int j=0; j<d; ++j){
+      int day = a*j+1;
+      if(day > d) break;
+      ++eat;
+    }
+  }
+  cout << eat+x << endl;
 }
