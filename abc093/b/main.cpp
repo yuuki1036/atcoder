d040ad3dd99a0d4473fe38581637a34339061a0e
yuuki1036 <yuuki1036@gmail.com>
2021-03-04 23:45:09 +0900

    backup

--- abc093/b/main.cpp
+++ abc093/b/main.cpp
@@ -1,8 +1,10 @@
 #include <bits/stdc++.h>
 using namespace std;
 int main(){
-  
-
-
-
+  int a, b, k;
+  cin >> a >> b >> k;
+  set<int>S;
+  for(int i=a; i<a+k; ++i) if(a <= i && i <= b) S.insert(i);
+  for(int i=b-k+1; i<=b; ++i) if(a <= i && i <= b) S.insert(i);
+  for(auto s : S) cout << s << endl;
 }
