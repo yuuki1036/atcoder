c725fc1e93f8ce3feee38adecde64dde857df860
yuuki1036 <yuuki1036@gmail.com>
2021-03-14 23:55:13 +0900

    backup

--- abc094/b/main.cpp
+++ abc094/b/main.cpp
@@ -1,7 +1,14 @@
 #include <bits/stdc++.h>
 using namespace std;
 int main(){
-  
+  int n, m, x;
+  cin >> n >> m >> x;
+  int a[m];
+  for(int i=0; i<m; ++i) cin >> a[i];
+  int ans = 0;
+  for(int i=x; i<n+1; ++i){
+    if(a[i])
+  }
 
 
 
--- abc098/a/main.cpp
+++ abc098/a/main.cpp
@@ -1,8 +1,8 @@
 #include <bits/stdc++.h>
 using namespace std;
 int main(){
-  
-
-
-
+  int a, b;
+  cin >> a >> b;
+  int ans = -1e9;
+  cout << max(max(max(ans, a+b), a-b), a*b) << endl;
 }
