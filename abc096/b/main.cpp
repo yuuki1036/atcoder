b01a35ec10efbb80d279e93fde213e033c0c3001
yuuki1036 <yuuki1036@gmail.com>
2021-03-18 22:59:37 +0900

    backup

--- abc096/b/main.cpp
+++ abc096/b/main.cpp
@@ -1,8 +1,11 @@
 #include <bits/stdc++.h>
 using namespace std;
+using ll = long long;
 int main(){
-  
-
-
-
+  int a, b, c, k;
+  cin >> a >> b >> c >> k;
+  int mx = max(max(a, b), c);
+  ll twice = mx;
+  for(int i=0; i<k; ++i) twice *= 2;
+  cout << twice+a+b+c-mx << endl;
 }
