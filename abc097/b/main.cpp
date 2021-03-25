acdf7736e24a3f2582f9495e08f518fc3411940f
yuuki1036 <yuuki1036@gmail.com>
2021-03-25 19:40:04 +0900

    backup

--- abc097/b/main.cpp
+++ abc097/b/main.cpp
@@ -1,8 +1,16 @@
 #include <bits/stdc++.h>
 using namespace std;
+using ll = long long;
 int main(){
-  
-
-
-
+  int x;
+  cin >> x;
+  ll ans = 1;
+  for(int i=1; i<=100; ++i){
+    for(int j=2; j<=10; ++j){
+      long long res = pow(i, j);
+      if(res <= x) ans = max(ans, res);
+      else break;
+    }
+  }
+  cout << ans << endl;
 }
