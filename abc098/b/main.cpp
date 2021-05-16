3311edb4d4b84b2debe91ae0a61631fdfc17836f
yuuki1036 <yuuki1036@gmail.com>
2021-05-16 20:23:30 +0900

    backup

--- abc098/b/main.cpp
+++ abc098/b/main.cpp
@@ -1,8 +1,35 @@
 #include <bits/stdc++.h>
 using namespace std;
-int main(){
-  
-
-
-
+int main()
+{
+  int n;
+  string s;
+  cin >> n >> s;
+  int ans = 0;
+  for (int i = 1; i < n; ++i)
+  {
+    int both = 0;
+    map<char, int> m;
+    for (int r = 0; r < i; ++r)
+    {
+      if (m.find(s[r]) == m.end())
+      {
+        m[s[r]] = 0;
+      }
+    }
+    for (int l = i; l < n; ++l)
+    {
+      if (m.find(s[l]) != m.end())
+      {
+        m[s[l]] = 1;
+      }
+    }
+    for (auto &[k, v] : m)
+    {
+      if (v > 0)
+        ++both;
+    }
+    ans = max(ans, both);
+  }
+  cout << ans << endl;
 }
