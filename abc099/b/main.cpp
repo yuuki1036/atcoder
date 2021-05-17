2a1915198199269d09d017c0ae972b6d4da76288
yuuki1036 <yuuki1036@gmail.com>
2021-05-17 22:51:12 +0900

    backup

--- abc099/b/main.cpp
+++ abc099/b/main.cpp
@@ -1,8 +1,18 @@
 #include <bits/stdc++.h>
 using namespace std;
-int main(){
-  
-
-
-
+int main()
+{
+  int a, b;
+  cin >> a >> b;
+  int r = b - a;
+  int l = 0;
+  for (int i = 1; i < 1000; ++i)
+  {
+    l += i;
+    if (i == r)
+    {
+      cout << l - i - a << endl;
+      return 0;
+    }
+  }
 }
