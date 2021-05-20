548e6cd5172efe48cd91cfff0312fdb67efa722d
yuuki1036 <yuuki1036@gmail.com>
2021-05-20 23:56:50 +0900

    backup

--- abc090/c/main.cpp
+++ abc090/c/main.cpp
@@ -1,8 +1,17 @@
 #include <bits/stdc++.h>
 using namespace std;
-int main(){
-  
-
-
-
+using ll = long long;
+int main()
+{
+  ll n, m, ans;
+  cin >> n >> m;
+  if (n == 1 && m == 1)
+    ans = 1;
+  else if (n == 1 && m != 1)
+    ans = m - 2;
+  else if (n != 1 && m == 1)
+    ans = n - 2;
+  else
+    ans = (n - 2) * (m - 2);
+  cout << ans << endl;
 }
