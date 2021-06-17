ab6fcd282cdfe4cc0f901b249705b37f3ea47a67
yuuki1036 <yuuki1036@gmail.com>
2021-06-17 23:15:00 +0900

    backup

--- abc100/b/main.cpp
+++ abc100/b/main.cpp
@@ -1,8 +1,9 @@
 #include <bits/stdc++.h>
 using namespace std;
 int main(){
-  
-
-
-
+  int d, n;
+  cin >> d >> n;
+  int ans = n*pow(100, d);
+  int ans2 = ans+pow(100, d);
+  cout <<( n == 100 ? ans2 : ans )<< endl;
 }
