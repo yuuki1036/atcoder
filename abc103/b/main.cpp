5dd3ab969185d296e990e5f89e26c5c23c7b62d6
yuuki1036 <yuuki1036@gmail.com>
2021-07-02 00:00:53 +0900

    backup

--- abc103/b/main.cpp
+++ abc103/b/main.cpp
@@ -1,8 +1,17 @@
 #include <bits/stdc++.h>
 using namespace std;
 int main(){
-  
-
-
-
+  string S, T;
+  cin >> S >> T;
+  int l = S.length();
+  for(int i=0; i<l; ++i){
+    if (S == T) {
+      cout << "Yes" << endl;
+      return 0;
+    }
+    char e = S.at(l-1);
+    S.pop_back();
+    S = e + S;
+  }
+  cout << "No" << endl;
 }
